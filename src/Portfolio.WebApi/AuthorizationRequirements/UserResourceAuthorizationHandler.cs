547784b9f1048ea682ac22ce5b82db760a5a1c88
guidoglielmi <guidoglielmi@gmail.com>
2022-12-17 19:12:21 -0300

    added necessary files

---
+++ src/Portfolio.WebApi/AuthorizationRequirements/UserResourceAuthorizationHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Portfolio.WebApi.DTO.EducationDtos;
+using System.Security.Claims;
+
+namespace Portfolio.WebApi.AuthorizationRequirements;
+public class UserResourceAuthorizationHandler : AuthorizationHandler<UserResourceRequirement, EducationPutDto>
+{
+  private readonly ILogger _logger;
+
+  /*
+An authorization handler is responsible for the evaluation of a requirement's properties.
+The authorization handler evaluates the requirements against a provided AuthorizationHandlerContext to determine if access is allowed. 
+Alternatively, a handler may implement IAuthorizationHandler directly to handle more than one type of requirement.
+*/
+  public UserResourceAuthorizationHandler(ILoggerFactory loggerFactory)
+      => _logger = loggerFactory.CreateLogger(GetType().FullName);
+  protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserResourceRequirement requirementToAccessResource, EducationPutDto protectedResource)
+  {
+    var userFullName = context.User.Claims
+      .Where(c => c.Type is ClaimTypes.Name or ClaimTypes.Surname)
+      .Select(c => c.Value)
+      .Aggregate((p, c) => p + " " + c);
+
+    _logger.LogInformation("{userFullName} tried to log in at {currentTime}", userFullName, DateTime.UtcNow.ToShortTimeString());
+
+    var idClaim = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.PrimarySid);
+    if (idClaim == null)
+    {
+      return Task.CompletedTask;
+    }
+    var resourceBelongToRequestingUser = idClaim.Value == protectedResource.UserId.ToString();
+    var userIsAdmin = context.User.IsInRole(UserResourceRequirement.Policy);
+    if (resourceBelongToRequestingUser && userIsAdmin!)
+    {
+      context.Succeed(requirementToAccessResource);
+    }
+    return Task.CompletedTask;
+  }
+}
---
+++ src/Portfolio.WebApi/AuthorizationRequirements/UserResourceRequirement.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.Extensions.Options;
+
+namespace Portfolio.WebApi.AuthorizationRequirements;
+// the name of the class must correspond to a handler that is used for this policy
+public class UserResourceRequirement : IAuthorizationRequirement
+{
+  // a requirement is used mainly to pass parameters when annotating with the authorize class
+  public string Name { get; set; }
+  public UserResourceRequirement()
+  {
+
+  }
+  // a constructor can be used to pass values used in authorization
+  public const string Policy = "UserResource";
+}
+
+public class SampleAuthorizationMiddlewareResultHandler : IAuthorizationMiddlewareResultHandler
+{
+  private readonly IAuthorizationMiddlewareResultHandler _defaultHandler;
+
+  public SampleAuthorizationMiddlewareResultHandler(IAuthorizationMiddlewareResultHandler defaultHandler)
+  {
+    _defaultHandler = defaultHandler;
+  }
+  public async Task HandleAsync(
+      RequestDelegate next,
+      HttpContext context,
+      AuthorizationPolicy policy,
+      PolicyAuthorizationResult authorizeResult)
+  {
+    // If the authorization was forbidden and the resource had a specific requirement,
+    // provide a custom 404 response.
+    if (authorizeResult.Forbidden
+        && authorizeResult.AuthorizationFailure!.FailedRequirements
+            .OfType<UserResourceRequirement>()
+            .Any())
+    {
+      // Return a 404 to make it appear as if the resource doesn't exist.
+      context.Response.StatusCode = StatusCodes.Status404NotFound;
+      return;
+    }
+
+    // Fall back to the default implementation.
+    await _defaultHandler.HandleAsync(next, context, policy, authorizeResult);
+  }
+}
+
+
+
+
+
+
+
+public static class Operations
+{
+  public static OperationAuthorizationRequirement Create =
+      new()
+      { Name = nameof(Create) };
+  public static OperationAuthorizationRequirement Read =
+      new()
+      { Name = nameof(Read) };
+  public static OperationAuthorizationRequirement Update =
+      new()
+      { Name = nameof(Update) };
+  public static OperationAuthorizationRequirement Delete =
+      new()
+      { Name = nameof(Delete) };
+}
+
+public class MinimumAgeAuthorizeAttribute : AuthorizeAttribute
+{
+  const string POLICY_PREFIX = "MinimumAge";
+  public MinimumAgeAuthorizeAttribute(int age) => Age = age;
+  public int Age
+  {
+    get
+    {
+      return int.TryParse(Policy[POLICY_PREFIX.Length..], out var age) ? age : default;
+    }
+    set => Policy = $"{POLICY_PREFIX}{value}";
+    // this is where the policy name, from the Authorize attribute, is set.
+    // this will be checked against the registered policies to match the corresponding built policy.
+    // alternatively, the policy will be picked up by the policy provider to
+    // build the related policy
+  }
+}
+
+public class MinimumAgeRequirement : IAuthorizationRequirement
+{
+  public MinimumAgeRequirement(int minimumAge) =>
+      MinimumAge = minimumAge;
+
+  public int MinimumAge { get; }
+}
+
+internal class MinimumAgePolicyProvider : IAuthorizationPolicyProvider
+{
+  // Used to retrieve authorization policies.
+  //ASP.NET Core only uses one instance of IAuthorizationPolicyProvider.
+  //If a custom provider isn't able to provide authorization policies for all policy names that will be used,
+  //it should defer to a backup provider.
+
+  /*
+  To use custom policies from an IAuthorizationPolicyProvider, you must:
+    - Register the appropriate AuthorizationHandler types with dependency injection, as with all policy-based authorization scenarios.
+    - Register the custom IAuthorizationPolicyProvider type to replace the default policy provider.
+  */
+
+  // it builds all the necessary policies from each authorized attribute used
+  // [Authorize("MinimumAge21")]
+  // [Authorize("MinimumAge18")]
+  // [Authorize("MinimumAge13")]
+  // ...
+  // this is done to avoid having to add the authorization policies by hand:
+  // services.AddAuthorization(opt =>
+  // {
+  //   opt.AddPolicy("policy1", policy => policy.Requirements.Add(new UserResourceRequirement(1)));
+  //   opt.AddPolicy("policy2", policy => policy.Requirements.Add(new UserResourceRequirement(2)));
+  //   ...
+  // });
+  // instead, when [Authorize("MinimumAge<age>")] or [MinimumAge(age)] is used, the policy is added programatically
+  const string POLICY_PREFIX = "MinimumAge";
+
+  public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+  {
+    //a custom IAuthorizationPolicyProvider needs to implement GetDefaultPolicyAsync
+    //to provide an authorization policy for [Authorize] attributes without a policy name specified.
+    return Task.FromResult(new AuthorizationPolicyBuilder(CookieAuthenticationDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build());
+  }
+
+  public Task<AuthorizationPolicy> GetFallbackPolicyAsync()
+  {
+    throw new NotImplementedException();
+  }
+  public DefaultAuthorizationPolicyProvider BackupPolicyProvider { get; }
+  public MinimumAgePolicyProvider(IOptions<AuthorizationOptions> options)
+  {
+    // ASP.NET Core only uses one authorization policy provider, so if the custom implementation
+    // doesn't handle all policies it should fall back to an alternate provider.
+    BackupPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+  }
+
+  public Task<AuthorizationPolicy> GetPolicyAsync(string policyName) // MinimumAge21
+  {
+    if (policyName.StartsWith(POLICY_PREFIX, StringComparison.OrdinalIgnoreCase) &&
+        int.TryParse(policyName[POLICY_PREFIX.Length..], out var age))
+    {
+      var policy = new AuthorizationPolicyBuilder(CookieAuthenticationDefaults.AuthenticationScheme);
+      policy.AddRequirements(new MinimumAgeRequirement(age));
+      return Task.FromResult(policy.Build());
+    }
+
+    return BackupPolicyProvider.GetPolicyAsync(policyName); // return backup policy
+  }
+}
